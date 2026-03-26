a267063632b5900e12ae6362d492a567135091ea
thalysonblack <thalysonrs@gmail.com>
2026-03-26 02:09:26 -0300

    fix: use client wrapper for Sanity Studio to resolve ssr:false and dynamic name conflict

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,16 +1,10 @@
 "use no memo";
 
-import dynamic from "next/dynamic";
-import config from "../../../../sanity.config";
-
 export { metadata, viewport } from "next-sanity/studio";
 export const dynamic = "force-dynamic";
 
-const NextStudio = dynamic(
-  () => import("next-sanity/studio").then((mod) => ({ default: mod.NextStudio })),
-  { ssr: false }
-);
+import StudioClient from "@/components/StudioClient";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <StudioClient />;
 }

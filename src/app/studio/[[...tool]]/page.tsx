0826a5a71e6cc4a0bdf7c67219b8de63fcd79b82
thalysonblack <thalysonrs@gmail.com>
2026-03-26 02:02:55 -0300

    fix: load Sanity Studio client-side only to avoid styled-components SSR crash

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,15 @@
 "use no memo";
 
-import { NextStudio } from "next-sanity/studio";
+import dynamic from "next/dynamic";
 import config from "../../../../sanity.config";
 
 export { metadata, viewport } from "next-sanity/studio";
 export const dynamic = "force-dynamic";
+
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((mod) => ({ default: mod.NextStudio })),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
